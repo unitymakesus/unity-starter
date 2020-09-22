e4a0c381bd2e7c705ae1fc11ea14aaebc0c32791
Andy Marino <andy@unitywebagency.com>
2020-09-22 16:22:25 -0400

    uncomment dom routes in js
    These are needed out of the box, we can remove unused DOM routes in the project.

--- wp-content/themes/unity-child/resources/assets/scripts/main.js
+++ wp-content/themes/unity-child/resources/assets/scripts/main.js
@@ -1,25 +1,24 @@
 // import external dependencies
 import 'jquery';
 import 'custom-event-polyfill';
-// import 'materialize-css';
 
 // Import everything from autoload
 import './autoload/*';
 
 // import local dependencies
-// import Router from './util/Router';
-// import common from './routes/common';
-// import home from './routes/home';
-// import aboutUs from './routes/about';
-// import archive from './routes/archive';
+import Router from './util/Router';
+import common from './routes/common';
+import home from './routes/home';
+import aboutUs from './routes/about';
+import archive from './routes/archive';
 
 /** Populate Router instance with DOM routes */
-// const routes = new Router({
-//   common,
-//   home,
-//   aboutUs,
-//   archive,
-// });
+const routes = new Router({
+  common,
+  home,
+  aboutUs,
+  archive,
+});
 
 /** Load Events */
-// jQuery(document).ready(() => routes.loadEvents());
+jQuery(document).ready(() => routes.loadEvents());

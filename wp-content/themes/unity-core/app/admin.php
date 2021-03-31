3251050b386f3fe0fa3f99b55c8601f30340c8a3
Andrew Marino <andrew@andrewmarino.net>
2021-03-31 13:24:28 -0400

    remove site health widget from wp dashboard

--- wp-content/themes/unity-core/app/admin.php
+++ wp-content/themes/unity-core/app/admin.php
@@ -3,19 +3,10 @@
 namespace App;
 
 /**
-* Theme customizer
-*/
-add_action('customize_register', function (\WP_Customize_Manager $wp_customize) {
-
-});
-
-/**
-* Customizer JS
-*/
-add_action('customize_preview_init', function () {
-  // wp_enqueue_script('sage/customizer.js', asset_path('scripts/customizer.js'), ['customize-preview'], null, true);
-});
-
-add_action('customize_controls_enqueue_scripts', function () {
-  // wp_enqueue_script('sage/customizer-panel.js', asset_path('scripts/customizer-panel.js'), [], null, true);
+ * Clean up the dashboard.
+ *
+ * @return void
+ */
+add_action('wp_dashboard_setup', function () {
+    remove_meta_box('dashboard_site_health', 'dashboard', 'normal');
 });

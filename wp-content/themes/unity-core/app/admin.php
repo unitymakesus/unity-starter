16eae5ac3085e1e5cf9cb8fa9c9d2ff6abdd7e37
Andrew Marino <andrew@andrewmarino.net>
2021-03-31 13:24:51 -0400

    disable auto-updates for plugins and themes

--- wp-content/themes/unity-core/app/admin.php
+++ wp-content/themes/unity-core/app/admin.php
@@ -10,3 +10,11 @@ namespace App;
 add_action('wp_dashboard_setup', function () {
     remove_meta_box('dashboard_site_health', 'dashboard', 'normal');
 });
+
+/**
+ * Disable auto-updates for plugins and themes.
+ *
+ * @since 5.5
+ */
+add_filter('plugins_auto_update_enabled', '__return_false');
+add_filter('themes_auto_update_enabled', '__return_false');

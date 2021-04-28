ee76535585e1c9764e2465ea2ca9f8af638f9c8b
Andrew Marino <andrew@andrewmarino.net>
2021-04-28 09:50:58 -0400

    remove update_nag if managewp is active

--- wp-content/themes/unity-core/app/admin.php
+++ wp-content/themes/unity-core/app/admin.php
@@ -18,3 +18,13 @@ add_action('wp_dashboard_setup', function () {
  */
 add_filter('plugins_auto_update_enabled', '__return_false');
 add_filter('themes_auto_update_enabled', '__return_false');
+
+/**
+ * Disable WP update notice(s) if ManageWP is active.
+ */
+add_action('admin_head', function () {
+    if (is_plugin_active('worker/init.php')) {
+        remove_action('admin_notices', 'update_nag', 3);
+        remove_action('network_admin_notices', 'update_nag', 3);
+    }
+});
